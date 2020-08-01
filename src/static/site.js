fe624843736fe27c96394c121a207fb525e518b9
Christopher Pritchard <chrispritchard.nz@gmail.com>
2020-08-01 17:32:57 +1200

    today display setup

--- src/static/site.js
+++ src/static/site.js
@@ -10,6 +10,23 @@ function getResponse(path, onResult) {
     request.send();
 }
 
+getResponse("/today", function(today) {
+    if (today.Weight && today.Weight != 0) {
+        document.querySelector("#recorded-weight").innerText == "Today's Weight: "+today.Weight + " KG";
+    } else {
+        document.querySelector("#set-weight-button").classList.remove("hide");
+    }
+
+    var totalConsumed = 0;
+    var entries = document.querySelector("#today-entries");
+    for(var i = 0; i < today.Calories; i++) {
+        var entry = today.Calories[i];
+        totalConsumed += entry.Amount;
+        entries.innerHTML += "<li>"+entry.Amount+" Cal - "+entry.Category+"</li>";
+    }
+    document.querySelector("#total-consumed").innerText = "Total Consumed Today: "+totalConsumed+" Cal";
+});
+
 getResponse("/categories", function(categories) {
     var select = document.querySelector("#categories");
     for (var i = 0; i < categories.length; i++) {
