3e67cd8b8892957e1a51b11b719b90da6b1a718d
Christopher <chrispritchard.nz@gmail.com>
2020-09-01 20:51:09 +1200

    reworked trend handler to return weighted

--- src/handlers.go
+++ src/handlers.go
@@ -334,10 +334,10 @@ func historyHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-type trendModel struct {
-	lastTwoWeeks      []recordedDay
-	trend             float64
-	averageCalsPerDay float64
+type trendEntry struct {
+	date     string
+	recorded float64
+	weighted float64
 }
 
 func trendHandler(w http.ResponseWriter, r *http.Request) {
@@ -349,46 +349,31 @@ func trendHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var lastTwoWeeks []recordedDay
-	if len(allEntries) == 0 {
-		writeTrendResponse(w, r, []recordedDay{}, 0, 0)
-		return
-	} else if len(allEntries) < 14 {
-		lastTwoWeeks = allEntries
-	} else {
-		lastTwoWeeks = allEntries[len(allEntries)-14:]
-	}
+	result := make([]trendEntry, 0)
+	var lastTwoWeeks []float64
 
-	trend := (lastTwoWeeks[len(lastTwoWeeks)-1].weight - lastTwoWeeks[0].weight) / float64(len(lastTwoWeeks))
-	trend = math.Round(trend*100) / 100
-
-	var totalCals float64
-	for _, day := range lastTwoWeeks {
-		for _, entry := range day.entries {
-			totalCals += float64(entry.Amount)
+	for _, day := range allEntries {
+		entry := trendEntry{day.date, day.weight, 0.0}
+		if len(lastTwoWeeks) == 14 {
+			lastTwoWeeks = append(lastTwoWeeks[1:], day.weight)
+		} else {
+			lastTwoWeeks = append(lastTwoWeeks, day.weight)
 		}
+		sum := 0.0
+		for _, weight := range lastTwoWeeks {
+			sum += weight
+		}
+		entry.weighted = math.Round((sum/float64(len(lastTwoWeeks)))*100) / 100
+		result = append(result, entry)
 	}
 
-	averageCalsPerDay := totalCals / float64(len(lastTwoWeeks))
-	averageCalsPerDay = math.Round(averageCalsPerDay*100) / 100
-
-	writeTrendResponse(w, r, lastTwoWeeks, trend, averageCalsPerDay)
-}
-
-func writeTrendResponse(w http.ResponseWriter, r *http.Request, lastTwoWeeks []recordedDay, trend, averageCalsPerDay float64) {
-	result := trendModel{lastTwoWeeks, trend, averageCalsPerDay}
 	contentType := r.Header.Get("Content-type")
 	if contentType == "application/json" {
 		w.Header().Set("Content-Type", contentType)
 		json.NewEncoder(w).Encode(result)
 	} else {
-		fmt.Fprintln(w, result.trend)
-		fmt.Fprintln(w, result.averageCalsPerDay)
-		for _, day := range result.lastTwoWeeks {
-			fmt.Fprintf(w, "%s %f\n", day.date, day.weight)
-			for _, entry := range day.entries {
-				fmt.Fprintf(w, "%d\t%s\n", entry.Amount, entry.Category)
-			}
+		for _, entry := range result {
+			fmt.Fprintf(w, "%s\n%f\n%f\n\n", entry.date, entry.recorded, entry.weighted)
 		}
 	}
 }
